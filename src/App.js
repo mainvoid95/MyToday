f676a56b3adaaf38cb1a4fb0920594a73334fc19
정종헌 <heonmac@jeongjongheon-ui-MacBookPro.local>
2019-09-28 00:57:56 +0900

    생활코딩 리엑트 강의 수강 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React , {Component} from 'react';
 import Navi from './component/Navi';
-import Content from './component/Content';
+import ReadContent from './component/ReadContent';
+import CreateContent from './component/CreateContent';
+import UpdateContent from './component/UpdateContent';
 import Subject from './component/Subject';
+import Control from './component/Control';
 import './App.css';
 
 
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode:'welcome',
       selected_content_id:2,
@@ -20,23 +24,63 @@ class App extends Component {
       ]
     }
   }
-  render(){
-    var _title, _desc = null;
+  getReadContent(){
+    var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          return data;
+        }
+        i = i + 1;
+      }
+  }
+  getContent(){
+    var _title, _desc, _article, _content = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode === 'read'){
-        var i = 0;
-        while(i < this.state.contents.length){
-          var data = this.state.contents[i];
-          if(data.id === this.state.selected_content_id){
-            _title = data.title;
-            _desc = data.desc;      
-            break;
+      _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    }else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={
+        (_title, _desc)=>{
+          this.max_content_id = this.max_content_id + 1; 
+          const _contents = Array.from(this.state.contents);
+          _contents.push(
+            {id:this.max_content_id, title:_title, desc:_desc}
+          )
+          this.setState({
+            contents:_contents,
+            mode:'read',
+            selected_content_id:this.max_content_id
+          });
+        } 
+      }></CreateContent>
+    }else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+        (_id ,_title, _desc)=>{
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id){
+              _contents[i] = {id:_id, title:_title, desc:_desc}
+              break;
+            }
+            i = i + 1;
           }
-          i = i + 1;
-        }
-      }
+          this.setState({
+            contents:_contents,
+            mode:'read'
+          });
+        } 
+      }></UpdateContent >
+    }
+    return _article;
+  }
+  render(){
     return (
       <div className="App">
         <Subject 
@@ -58,7 +102,33 @@ class App extends Component {
             })
           }
         } data={this.state.contents}></Navi>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={
+          (_mode)=>{
+            if(_mode === 'delete'){
+              if(window.confirm('삭제하시겠습니까?')){
+                var i = 0;
+                const _contents = Array.from(this.state.contents);
+                while(i < _contents.length){
+                  if(_contents[i].id === this.state.selected_content_id){
+                    _contents.splice(i,1);
+                    break;
+                  }
+                  i = i + 1;
+                }
+                this.setState({
+                  mode:'welcome',
+                  contents:_contents,
+                })
+                this.max_content_id = this.max_content_id -1;
+              }
+            }else{
+              this.setState({
+                mode:_mode,
+              })
+            }
+          }
+        }></Control>
+        {this.getContent()}
       </div>
     );
   }
