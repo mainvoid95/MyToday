ce6ad03cb71c31cc04cf6401697e8bf78ebed23d
정종헌 <mainvoid95@gmail.com>
2020-01-29 17:49:30 +0900

    ui test

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Link, Redirect, Switch, Route, BrowserRouter as Router} from "react-rou
 import './App.css';
 import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+
 import {get, post} from 'axios';
 import Home from './components/Home';
 import UsersRegister from './components/UserRegister';
@@ -17,7 +18,9 @@ import Popup from 'react-popup';
 import github from './GitHub.png';
 import mail from './mail.png';
 
-const { Header, Footer, Sider, Content } = Layout;
+// antd 관련 상수들 (ui관련된것들)
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 
 class App extends React.Component{
@@ -30,9 +33,14 @@ class App extends React.Component{
       user_number:'',
       user_name:'',
       user_email:'',
+      collapsed: false,
     }
   }
 
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
 
   stateRefresh = () => {
     this.setState({
@@ -178,11 +186,75 @@ class App extends React.Component{
       )
     }
     return(
-      <div>
+      <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1">
+            <Icon type="pie-chart" />
+            <span>Option 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="desktop" />
+            <span>Option 2</span>
+          </Menu.Item>
+          <SubMenu
+            key="sub1"
+            title={
+              <span>
+                <Icon type="user" />
+                <span>User</span>
+              </span>
+            }
+          >
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            title={
+              <span>
+                <Icon type="team" />
+                <span>Team</span>
+              </span>
+            }
+          >
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9">
+            <Icon type="file" />
+            <span>File</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
       <Layout>
+        <Header style={{ background: '#fff', padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+      
+    )
+  }
+}
+
+
+export default App;
+
+
+
+{/* <Layout>
         <Header>
           <header>
-                {this.loginedNav()}
+                  {this.loginedNav()}
                   <nav className='clocknav'>
                       <Clock/>
                       <Weather/>
@@ -210,13 +282,4 @@ class App extends React.Component{
         <a href="mailto:mainvoid95@gmail.com?Subject=문의사항" target="_top"><img src={mail} className='mailImg'/></a>
         <a href="https://github.com/mainvoid95/MyToday"><img src={github}className='GithubImg' /></a>
       </Footer>
-      </Layout>
-      </div>
-      
-    )
-  }
-}
-
-
-export default App;
-
+      </Layout> */}
